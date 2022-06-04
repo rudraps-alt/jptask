33cacd46783b6450219f8161a43d9f45589369d2
esha123802 <76272081+esha123802@users.noreply.github.com>
2022-06-04 19:16:14 +0530

    connection to a database

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
-const share = require('/routes/share')
+const share = require('./routes/share')
+const dotenv = require("dotenv")
 
 app.use(express.urlencoded({extended : false}))
 app.use(express.json())
 app.use('/share', share)
+
+dotenv.config()
+
+const connectDB = require('./config/db')
+
+connectDB()
 
 app.listen(5000)
 
